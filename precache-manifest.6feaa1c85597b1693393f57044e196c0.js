d6162a12073cfbf654a7e494a34e558a26e94e16
Paul Bradley <bradleypauld@gmail.com>
2020-09-05 20:06:39 -0500

    Updates

--- precache-manifest.6feaa1c85597b1693393f57044e196c0.js
+++ precache-manifest.6feaa1c85597b1693393f57044e196c0.js
@@ -1,51 +1,55 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ed366fba820021b8c8dbb1adaa8184b9",
+    "revision": "b32bd667634e82195c3f69963d7746b1",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "9bab9c9639bdd60cf351",
+    "revision": "e7447eb5dc35babbe7fc",
     "url": "/react-portfolio/static/css/main.dc6cb22d.chunk.css"
   },
   {
-    "revision": "2fda5a5ba403996472e9",
-    "url": "/react-portfolio/static/js/2.472ede12.chunk.js"
+    "revision": "b97a34837647161dfba1",
+    "url": "/react-portfolio/static/js/2.8b0f2af8.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react-portfolio/static/js/2.472ede12.chunk.js.LICENSE.txt"
+    "url": "/react-portfolio/static/js/2.8b0f2af8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9bab9c9639bdd60cf351",
-    "url": "/react-portfolio/static/js/main.460d1b3f.chunk.js"
+    "revision": "e7447eb5dc35babbe7fc",
+    "url": "/react-portfolio/static/js/main.47e5bad8.chunk.js"
   },
   {
     "revision": "aff33cef9dbb8787d6b3",
     "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
   },
   {
+    "revision": "012f004723fdeaeecadb331db8cd5ba5",
+    "url": "/react-portfolio/static/media/0.012f0047.png"
+  },
+  {
     "revision": "7c9d47b3e6cefb41e9a4ce40fb2542db",
-    "url": "/react-portfolio/static/media/0.7c9d47b3.png"
+    "url": "/react-portfolio/static/media/1.7c9d47b3.png"
   },
   {
     "revision": "1d902c8462c95769abaf65228657f7ad",
-    "url": "/react-portfolio/static/media/1.1d902c84.png"
+    "url": "/react-portfolio/static/media/2.1d902c84.png"
   },
   {
     "revision": "f1b05706a3176e45627dd28646614732",
-    "url": "/react-portfolio/static/media/2.f1b05706.png"
+    "url": "/react-portfolio/static/media/3.f1b05706.png"
   },
   {
     "revision": "f4f3e0ddd7ff1195ffca459da0bf9fa8",
-    "url": "/react-portfolio/static/media/3.f4f3e0dd.png"
+    "url": "/react-portfolio/static/media/4.f4f3e0dd.png"
   },
   {
     "revision": "593be821461ff4ac14713708407d9266",
-    "url": "/react-portfolio/static/media/4.593be821.png"
+    "url": "/react-portfolio/static/media/5.593be821.png"
   },
   {
     "revision": "f907cc68f31b2a7c4745310227fda07f",
-    "url": "/react-portfolio/static/media/5.f907cc68.png"
+    "url": "/react-portfolio/static/media/6.f907cc68.png"
   },
   {
     "revision": "064ace4777ac86d7627f455f36975452",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.6cf27464b72772d840c0a343001fe97c.js"
+  "/react-portfolio/precache-manifest.6feaa1c85597b1693393f57044e196c0.js"
 );
 
 self.addEventListener('message', (event) => {
